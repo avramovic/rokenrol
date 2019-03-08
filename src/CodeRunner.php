e757a086fb9412d156777e6845ea4e63db1f4535
Nemanja Avramović <avramyu@gmail.com>
2019-03-08 02:43:59 +0100

    прослеђивање променљивих из пхп-а у рнр

--- src/CodeRunner.php
+++ src/CodeRunner.php
@@ -2,8 +2,16 @@
 
 class CodeRunner
 {
+    protected $variables;
+
+    public function assign($var, $value)
+    {
+        $this->variables[$var] = $value;
+    }
+
     public function run($php)
     {
+        extract($this->variables);
         eval($php);
     }
 }
